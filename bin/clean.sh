9e8b08336da4e49d4d1364feeb4b5b7a469ed109
nishkamravi2 <nishkamravi@gmail.com>
2015-08-13 15:42:12 -0700

    Update clean.sh

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,9 +1,3 @@
 rm -r build
 rm sparkaid.jar
 rm -rf ../output
-
-rm -r ../auto-config/console/bin		
-rm -r ../auto-config/dynamicallocation/bin	
-rm -r ../auto-config/core/bin		
-rm -r ../auto-config/streaming/bin	
-rm -r ../utils/bin	
