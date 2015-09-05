0d7f46f63d22da35b0b853c868252bc19120ae09
Ethan Chan <ethan.chan@ethanchan-MBP.local>
2015-09-04 17:03:20 -0700

    dealing merge conflict

--- bin/run_LOCAL_51398.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -d "build" ]; then
-  ./build.sh
-fi
-
-java -classpath sparkaid.jar console.src.main.ConfigurationConsole "$@"
-python ../optimizer/src/main/main.py
-rm tmp-code-file-path.txt
