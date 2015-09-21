77089b3ea4bd9ef6543105af4d496ee43efb96e3
nishkamravi2 <nishkamravi@gmail.com>
2015-09-20 20:18:32 -0700

    Update Common.java

--- auto-config/core/src/main/common/Common.java
+++ auto-config/core/src/main/common/Common.java
@@ -59,7 +59,7 @@ public class Common {
 		static double daemonMemoryMin = 0.5; //gb
 		static double workerMaxHeapsize = 4;
 		
-		static int numExecutorsPerNodeUpperBound = 4;
+		static int numExecutorsPerNodeUpperBound = 6;
 		static int numExecutorsPerNodeLowerBound = 2;
 		
 		static double parallelismFactor = 4;
